1765bdcd7427ad1f4ce82c4ac7049951d3508ead
Mabri <joaquinperez@liceovvh.cl>
2025-10-23 15:06:40 -0300

    12

--- bucles_while_for/static/js/script.js
+++ bucles_while_for/static/js/script.js
@@ -25,7 +25,7 @@ function ejecutarFor() {
     }
 }
 
-function ejecutarWhile2() {
+function ejecutarWhile1() {
     let numero = parseInt(prompt("Ingrese su numero: "))
     let count = 1;
     let resultado = "El resultado es: "
@@ -37,7 +37,7 @@ function ejecutarWhile2() {
     alert(resultado)
 }
 
-function ejecutarWhile3() {
+function ejecutarWhile2() {
     let numero = parseInt(prompt("Ingrese su numero: "))
     let count = numero;
     let resultado = "El resultado es: "
@@ -47,4 +47,30 @@ function ejecutarWhile3() {
         count--
     }
     alert(resultado)
+}
+
+function ejecutarWhile3() {
+    let suma = 0;
+    let numero = parseInt(prompt("Ingrese su numero: "))
+    let contador = 1;
+    let resultado = "Suma: 0";
+    while (contador <= numero) {
+        suma += contador;
+        resultado += ` + ${contador}`;
+        contador++
+    }
+    alert(`${resultado} = ${suma}`)
+}
+
+function ejecutarWhile4() {
+    let numero = parseInt(prompt("Ingresar numero: "));
+    let count = 1;
+    let resultado = "el resultado es: "
+    while (count <= numero) {
+        count++
+        if (count % 2 == 0) {
+            resultado += ` ${count}`
+        }
+    }
+    alert(` ${resultado}`)
 }
